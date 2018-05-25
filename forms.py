e0ebc12e4885d3f36f683de63656a8d7718fc2a5
Pete Schmitt <peter.r.schmitt@icloud.com>
2018-05-25 14:14:43 -0400

    mods up through section 4

--- forms.py
+++ forms.py
@@ -1,9 +1,16 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
-class SignupForm(Form):
-    first_name = StringField('First name')
-    last_name = StringField('Last name')
-    email = StringField('Email')
-    password = StringField('Password')
+class SignupForm(FlaskForm):
+    first_name = StringField('First name', 
+            validators=[DataRequired('Please enter your first name.')])
+    last_name = StringField('Last name', 
+            validators=[DataRequired('Please enter your last name.')])
+    email = StringField('Email', 
+            validators=[DataRequired('Please enter your email address.'),
+                Email('Please enter your valid email address.')])
+    password = StringField('Password', 
+            validators=[DataRequired('Please enter your password.'), 
+                Length(min=6, message='Passwords must be >= 6 characters.')])
     submit = SubmitField('Sign up')
--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
-from models import db
+from models import db, User
 from forms import SignupForm
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
 db.init_app(app)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app.secret_key = 'development-key'
 
@@ -22,7 +23,14 @@ def signup():
     form = SignupForm()
 
     if request.method == 'POST':
-        return "Success!"
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
+        else:
+            newuser = User(form.first_name.data, form.last_name.data,
+                           form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            return "Success!"
     elif request.method == 'GET':
         return render_template('signup.html', form=form)
 
